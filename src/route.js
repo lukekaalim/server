a4c14a6033f9b203518e21d17490196e6624e978
Luke Kaalim <lukekaalim@gmail.com>
2019-09-13 02:30:28 +1000

    remove 'standard' route, REST route is only route now

--- src/route.js
+++ src/route.js
@@ -1,24 +1,27 @@
 // @flow strict
 const url = require('url');
 /*::
-import type { HTTPIncomingRequest, HTTPOutgoingResponse } from './http';
+import type { HTTPMethod } from './http';
 */
 
 /*::
+export type RouteRequest = {
+  query: Map<string, string>,
+  headers: Map<string, string>,
+  body?: string,
+};
+
+export type RouteResponse = {
+  status: number,
+  headers: { [header: string]: string },
+  body: string,
+}
+
+export type RouteHandler = (request: RouteRequest) => Promise<RouteResponse> | RouteResponse
+
 export type Route = {
-  test: (inc: HTTPIncomingRequest) => boolean,
-  handler: (inc: HTTPIncomingRequest, res: HTTPOutgoingResponse) => Promise<void>,
+  method: HTTPMethod,
+  path: string,
+  handler: RouteHandler,
 };
 */
-
-const createRoute = (
-  test/*: (inc: HTTPIncomingRequest) => boolean*/,
-  handler/*: (inc: HTTPIncomingRequest, res: HTTPOutgoingResponse) => Promise<void>*/,
-)/*: Route*/ => ({
-  test,
-  handler,
-});
-
-module.exports = {
-  createRoute,
-};
