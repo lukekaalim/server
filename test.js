48ff7ed3a89b4122e8c9b0a5a6e05b0dcc111f6a
Luke Kaalim <lukekaalim@gmail.com>
2019-09-15 22:35:49 +1000

    Update tests to cover  new code

--- test.js
+++ test.js
@@ -1,17 +1,14 @@
 // @flow strict
 const { expectAll, emojiReporter, booleanReporter } = require('@lukekaalim/test');
 
-const { expectNoMatchingRoutes, expectMatchFirstRoute, expectDefaultHandleOnRouteThrow } = require('./src/listener.test');
-const { expectRestEndpoint } = require('./src/route.test');
-const { expectIntegration } = require('./test/integration.test');
+const { expectNoMatchingRoutes, expectMatchingRoutes } = require('./src/listener.test');
+//const { expectRestEndpoint } = require('./src/route.test');
+//const { expectIntegration } = require('./test/integration.test');
 
 const testLibrary = async () => {
   const expectation = expectAll('@lukekaalim/server', [
     expectNoMatchingRoutes,
-    expectMatchFirstRoute,
-    expectRestEndpoint,
-    expectIntegration,
-    expectDefaultHandleOnRouteThrow,
+    expectMatchingRoutes,
   ]);
   const assertion = await expectation.test();
   console.log(emojiReporter(assertion));
