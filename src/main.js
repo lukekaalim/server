321751174a04961d75fe030028eaad6e449a765f
Luke Kaalim <lkaalim@nine.com.au>
2020-12-09 13:53:49 +1100

    Fix missing type exports

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ export type * from './request';
 
 export type * from './resource';
 export type * from './route';
+export type * from './content';
 
 export type * from './listener';
 export type * from './rest';
@@ -24,10 +25,12 @@ module.exports = {
 
   ...require('./route'),
   ...require('./resource'),
+  ...require('./content'),
 
   ...require('./listener'),
   ...require('./rest'),
 
   ...require('./json'),
   ...require('./stream'),
+  ...require('./http'),
 };
